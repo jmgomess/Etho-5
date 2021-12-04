5a7b93b9ab1ac0eb810d76050c2ab0530ce6792f
jmgomess <janaina.gomes_@hotmail.com>
2021-12-04 14:19:58 -0300

    Atualizacao da tela round e jogadores

--- Round6/force-app/main/default/lwc/jogadorContainer/jogadorContainer.js
+++ Round6/force-app/main/default/lwc/jogadorContainer/jogadorContainer.js
@@ -1,6 +1,9 @@
-import { LightningElement, api } from 'lwc';
+import { api, LightningElement } from "lwc";
 
 export default class JogadorContainer extends LightningElement {
-    @api jogador;
-    @api index;
+  @api jogador;
+
+  get vivoMortoClass() {
+    return this.jogador.Morreu__c ? "morto" : "vivo";
+  }
 }
